aeb322845684e36f6edd43605327db21fa46e010
Natabagass <meganatabagas@gmail.com>
2024-09-05 08:44:10 +0700

    feat(data): adding new data

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
-tesss
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/image', (req, res) => {
+    const imageUrl = `${req.protocol}://${req.get('host')}/assets/gambar.jpeg`;
+    res.json({ url: imageUrl });
+});
+
+app.get('/video', (req, res) => {
+    const videoUrl = `${req.protocol}://${req.get('host')}/assets/goldengate.mp4`;
+    res.json({ url: videoUrl });
+});
+
+app.get('/audio', (req, res) => {
+    const audioUrl = `${req.protocol}://${req.get('host')}/assets/ringtone_mTeam.mp3`;
+    res.json({ url: audioUrl });
+});
+
+app.get('/text', (req, res) => {
+    const text = "This is a sample text returned directly as a string.";
+    res.send(text);
+});
+
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.listen(PORT, () => {
+    console.log(`Udah jalan bos`);
+});
