3fca6ad31dc472feacfef536acbc82e6aa911bec
Natabagass <meganatabagas@gmail.com>
2024-09-18 23:10:14 +0700

    change get route

--- server.js
+++ server.js
@@ -4,30 +4,24 @@ const cors = require('cors');
 
 const app = express();
 const PORT = 5000;
+
 app.use(cors());
 
-app.get('/image', (req, res) => {
-    const imageUrl = `${req.protocol}://${req.get('host')}/assets/gambar.jpeg`;
-    res.json({ url: imageUrl });
-});
-
-app.get('/video', (req, res) => {
-    const videoUrl = `${req.protocol}://${req.get('host')}/assets/goldengate.mp4`;
-    res.json({ url: videoUrl });
-});
-
-app.get('/audio', (req, res) => {
-    const audioUrl = `${req.protocol}://${req.get('host')}/assets/ringtone_mTeam.mp3`;
-    res.json({ url: audioUrl });
-});
-
-app.get('/text', (req, res) => {
-    const text = "This is a sample text returned directly as a string.";
-    res.send({ text: text });
+app.get('/media', (req, res) => {
+    const baseUrl = `${req.protocol}://${req.get('host')}/assets`;
+    
+    const responseData = {
+        image: `${baseUrl}/gambar.jpeg`,
+        video: `${baseUrl}/goldengate.mp4`,
+        audio: `${baseUrl}/ringtone_mTeam.mp3`,
+        text: "This is a sample text returned directly as a string."
+    };
+    
+    res.json(responseData);
 });
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.listen(PORT, () => {
-    console.log(`Udah jalan bos`);
+    console.log(`Server is running on port ${PORT}`);
 });
